806bc185badb9be44fb1ee77cce2c329d884c153
Cambalache <hello@cambalache.studio>
2026-05-26 17:08:34 -0300

    revert: no renombrar label nautishop_publicaciones

--- supabase/migrations/0058_rename_nautishop_label.sql
+++
@@ -1,4 +0,0 @@
--- Renombrar 'Venta / Alquiler de camas y amarras online' al nombre original corto.
-UPDATE pricing_features
-SET label = 'Publicaciones en NautiShop'
-WHERE id = 'nautishop_publicaciones';
